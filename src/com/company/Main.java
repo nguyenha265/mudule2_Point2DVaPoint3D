9c82251a7acd3124e2390cdd6475f7edb5b75e30
nguyenha265 <50904425+nguyenha265@users.noreply.github.com>
2019-07-13 13:33:57 +0700

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Point2D point2D = new Point2D(4,5);
-        Point3D point3D = new Point3D(5,6);
+        Point3D point3D = new Point3D(5,6,7);
         System.out.println(point2D.toString());
         System.out.println(point3D.toString());
     }
